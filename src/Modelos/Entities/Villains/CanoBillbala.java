efad26758430d8844f3042506f8ea27b9d66d33b
Julio Cesar A. A. Fuganti <juliocesarfuganti@gmail.com>
2025-05-29 14:00:41 -0300

    Tudo pra documentar 2

--- src/Modelos/Entities/Villains/CanoBillbala.java
+++ src/Modelos/Entities/Villains/CanoBillbala.java
@@ -2,18 +2,14 @@ package Modelos.Entities.Villains;
 
 import Auxiliar.Consts;
 import Auxiliar.Desenho;
-import Auxiliar.Posicao;
-import Controler.Tela;
 import Modelos.Entities.Villains.AuxiliarVillains.Billbala;
 import Modelos.Personagem;
-import java.awt.Graphics;
 import java.awt.Rectangle;
-import java.io.Serializable;
 
 public class CanoBillbala extends Personagem {
     private int iContaIntervalos;
     private Personagem f;
-    
+
     public CanoBillbala(String sNomeImagePNG) {
         super(sNomeImagePNG);
         this.bTransponivel = false;
@@ -25,10 +21,10 @@ public class CanoBillbala extends Personagem {
         super.autoDesenho();
 
         this.iContaIntervalos++;
-        if(this.iContaIntervalos == Consts.TIMER){
+        if (this.iContaIntervalos == Consts.TIMER) {
             this.iContaIntervalos = 0;
             Billbala f = new Billbala("billbala.png");
-            f.setPosicao(pPosicao.getLinha(),pPosicao.getColuna()-1);
+            f.setPosicao(pPosicao.getLinha(), pPosicao.getColuna() - 1);
             f.setMortal(true);
             Desenho.acessoATelaDoJogo().addPersonagem(f);
         }
@@ -37,15 +33,14 @@ public class CanoBillbala extends Personagem {
     public Personagem getBillBala() {
         return f;
     }
-    
+
     @Override
     public Rectangle getHitbox() {
         int cellSize = Consts.CELL_SIDE;
         return new Rectangle(
-            pPosicao.getColuna() * cellSize,
-            pPosicao.getLinha() * cellSize,
-            cellSize,
-            2 * cellSize
-        );
+                pPosicao.getColuna() * cellSize,
+                pPosicao.getLinha() * cellSize,
+                cellSize,
+                2 * cellSize);
     }
 }
